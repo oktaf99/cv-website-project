6d326adf429e41abecfbc40425b1bfa83b08b975
oktaf99 <oktaferi18@gmail.com>
2024-03-25 01:40:02 +0700

    update 25 march 2024

--- src/components/ModalProduct.jsx
+++ src/components/ModalProduct.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-
+import { Swiper, SwiperSlide } from "swiper/react";
 import { spareParts } from "../assets/data/index";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/component.css";
+
+// import required modules
+import { Navigation, Pagination } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const ModalProduct = ({ activeID, setShowModal }) => {
   const product = spareParts.find((product) => product.id === activeID);
@@ -18,7 +26,48 @@ const ModalProduct = ({ activeID, setShowModal }) => {
   //onClick={(e) => setShowModal(false)}
   return (
     <div className="overlay">
-      {/* <div className="modal-product">
+      <div className="modal-product">
+        <Swiper
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Navigation, Pagination]}
+          className="mySwiper modal-image"
+        >
+          <SwiperSlide>
+            <img src={product.image} alt="" className="main-img" />
+          </SwiperSlide>
+          <SwiperSlide >
+            <img src={product.image2} alt="" className="img-detail" />
+            <h4>Specification</h4>
+          </SwiperSlide>
+        </Swiper>
+
+        <div className="modal-body">
+          <h3>{product.name}</h3>
+          <p>{product.category}</p>
+          <div className="btn-modal">
+            <button
+              onClick={() => buttonHandler("https://wa.me/+6285811482792/")}
+            >
+              Book <FontAwesomeIcon icon={faPhone} />
+            </button>
+          </div>
+        </div>
+
+        <button className="closeBtn" onClick={() => setShowModal(false)}>
+          <FontAwesomeIcon icon={faXmark} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ModalProduct;
+
+{
+  /* <div className="modal-product">
         <div className="modal-image w-100 d-flex justify-content-center align-items-center">
           <img src={product.image} alt="" className="main-img" />
         </div>
@@ -39,36 +88,16 @@ const ModalProduct = ({ activeID, setShowModal }) => {
         <button className="closeBtn" onClick={() => setShowModal(false)}>
           <FontAwesomeIcon icon={faXmark} />
         </button>
-      </div> */}
+      </div> */
+}
 
-      <div className="modal-product">
-        <Carousel data-bs-theme="dark" className="modal-image w-100 h-100">
+{
+  /* <Carousel data-bs-theme="dark" className="modal-image w-100 h-100">
           <Carousel.Item className="text-center">
             <img src={product.image} alt="" className="main-img" />
           </Carousel.Item>
           <Carousel.Item className="text-center">
             <img src={product.image2} alt="" className="img-detail" />
           </Carousel.Item>
-        </Carousel>
-
-        <div className="modal-body">
-          <h3>{product.name}</h3>
-          <p>{product.category}</p>
-          <div className="btn-modal">
-            <button
-              onClick={() => buttonHandler("https://wa.me/+6285811482792/")}
-            >
-              Interesting ? <FontAwesomeIcon icon={faPhone} />
-            </button>
-          </div>
-        </div>
-
-        <button className="closeBtn" onClick={() => setShowModal(false)}>
-          <FontAwesomeIcon icon={faXmark} />
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export default ModalProduct;
+        </Carousel> */
+}
